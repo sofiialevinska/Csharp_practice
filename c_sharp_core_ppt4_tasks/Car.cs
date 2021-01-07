f88afc9513e68d46ab195ffbd1d458be7e07ca35
Sofiia Levinska <sofialevinska@gmail.com>
2021-01-07 19:54:01 +0200

    ppt4 tasks

--- c_sharp_core_ppt4_tasks/Car.cs
+++ c_sharp_core_ppt4_tasks/Car.cs
@@ -1,10 +1,76 @@
 ﻿using System;
 namespace c_sharp_core_ppt4_tasks
 {
+    //Define class Car with fields name, color, price and const field CompanyName          
     public class Car
     {
+        private string name;
+        private double price;
+        private const string CompanyName = "Sofiia Levinska";
+
+        //Constructor default                                       
         public Car()
         {
+        }
+
+        //Constructor with parameters
+        public Car(string name, string color, double price)
+        {
+            this.name = name;
+            Color = color;
+            this.price = price;
+        }
+
+        //property to access the color field
+        public string Color { get; set; }
+
+        //method Input () - to enter car data from the console
+        public void Input()
+        {
+            Console.Write("\nPlease enter car's name.\nCar's name = ");
+            this.name = Console.ReadLine();
+            Console.Write("Please enter car's color.\nCar's color = ");
+            Color = Console.ReadLine();
+            Console.Write("Please enter car's price.\nCar's price = ");
+            this.price = Int32.Parse(Console.ReadLine());
+        }
+
+        //method Print () - to output the machine data to the console
+        public void Print()
+        {
+            Console.Write("\nCar's name = {0};", this.name);
+            Console.Write(" Car's color = {0};", Color);
+            Console.WriteLine(" Car's price = {0:f1}", this.price);
+        }
+
+        //method ChangePrice (double x) - to change the price by x%
+        public void ChangePrice(double x)
+        {
+            if (x > 0)
+            {
+                price = price * (1 + x / 100);
+            }
+            else
+            {
+                price = price * (1 - Math.Abs(x) / 100);
+            }
+        }
+
+        //Overload the operator == for the class Car (cars - equal if the name and price are equal)
+        public static bool operator ==(Car car1, Car car2)
+        {
+            return ((car1.name == car2.name) && (car1.price == car2.price));
+        }
+
+        public static bool operator !=(Car car1, Car car2)
+        {
+            return ((car1.name != car2.name) && (car1.price != car2.price));
+        }
+
+        //Overload the method ToString() in the class Car, which returns a line with data about the car
+        public override string ToString()
+        {
+            return ("(" + name + "; " + Color.ToString() + "; " + price + ")");
         }
     }
 }
--- c_sharp_core_ppt4_tasks/Program.cs
+++ c_sharp_core_ppt4_tasks/Program.cs
@@ -6,7 +6,57 @@ namespace c_sharp_core_ppt4_tasks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Enter data about 3 cars - variant1
+            Car car1 = new Car("car1", "white", 98);
+            Car car2 = new Car("car2", "green", 200);
+            Car car3 = new Car("car1", "blue", 98);
+
+            ////Enter data about 3 cars - variant2
+            //Car car1 = new Car();
+            //Car car2 = new Car();
+            //Car car3 = new Car();
+
+            //car1.Input();
+            //car2.Input();
+            //car3.Input();
+
+            //Decrease cars prices by 10%, display info about the car.
+            double percentageToChangePrice = -10;
+            car1.ChangePrice(percentageToChangePrice);
+            car2.ChangePrice(percentageToChangePrice);
+            car3.ChangePrice(percentageToChangePrice);
+
+            car1.Print();
+            car2.Print();
+            car3.Print();
+
+            //Enter a new color and paint the car with the color white in the specified color
+            string newColor = "black";
+
+            Car[] cars = new Car[3] { car1, car2, car3 };
+            foreach (Car car in cars)
+            {
+                if (car.Color == "white")
+                {
+                    car.Color = newColor;
+                    car.Print();
+                }
+            }
+
+            //print cars that are equal
+            for (int i = 0; i < cars.Length - 1; i++)
+            {
+                for (int j = i + 1; j < cars.Length; j++)
+                {
+                    if (cars[i] == cars[j])
+                    {
+                        Console.WriteLine("\ncar{0} == car{1}", i + 1, j + 1);
+                    }
+                }
+            }
+
+            //print info about car1 using method ToString()
+            Console.WriteLine("\ncar1.ToString() = {0}", car1.ToString());
         }
     }
 }
