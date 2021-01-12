89b3a23701c756e22d057f9b01e6f7d1fbe35a1e
Sofiia Levinska <sofialevinska@gmail.com>
2021-01-12 17:34:08 +0200

    ppt7 task

--- c_sharp_core_ppt7_task/Program.cs
+++ c_sharp_core_ppt7_task/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace c_sharp_core_ppt7_task
 {
@@ -6,7 +7,31 @@ namespace c_sharp_core_ppt7_task
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string readPath = @"/Users/sofiiageletukha/Desktop/Coding/SETtest/hw/hw5/c_sharp_core/c_sharp_core_ppt7_task/data.txt";
+            string writePath = @"/Users/sofiiageletukha/Desktop/Coding/SETtest/hw/hw5/c_sharp_core/c_sharp_core_ppt7_task/rez.txt";
+            string text = "";
+            try
+            {
+                using (StreamReader sr = new StreamReader(readPath, System.Text.Encoding.Default))
+                {
+                    text = sr.ReadToEnd();
+                }
+
+                File.WriteAllText(writePath, text);
+
+                using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine("\nMore text More text More text");
+                    sw.WriteLine (" 1234 " + 1234 + " 1234 ");
+                }
+
+                string addText = "WriteAllText WriteAllText WriteAllText" + 5678 + " Five Six Seven Eight";
+                File.AppendAllText(writePath, addText);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
